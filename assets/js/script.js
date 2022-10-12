88280a70a0a87dbaecdd17c7697fc6671932310a
ocassidydev <ocassidy@tcd.ie>
2022-10-12 16:05:36 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,10 +48,12 @@ function checkAnswer(){
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if(isCorrect){
-        alert("Hey! You got it right! :D");
+        alert("Hey! You got it right :D")
+        incrementScore();
     }
     else {
-        alert(`Aww... You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        alert(`Aww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -75,12 +77,20 @@ function calculateCorrectAnswer(){
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore(){
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
